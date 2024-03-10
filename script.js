84bdf5935c9bce57890ddd7e93c6bd02d057eeab
Losara2023 <arabest2023@gmail.com>
2024-03-10 22:41:19 +0100

    add some comments

--- script.js
+++ script.js
@@ -2,38 +2,41 @@ const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
 
-
-function addItem(e){
-  e.preventDefault()
-  let newItem = itemInput.value
+function addItem(e) {
+  e.preventDefault();
   // Validate Input
-  if(newItem ==="" ){
-    alert("Please add an item!")
-    return
+  // Get Value typed in Input
+  const newItem = itemInput.value;
+  if (newItem === "") {
+    alert("Please add an item!");
+    return;
   }
-
-  // Create List item
+  // Create List Item
   const li = document.createElement("li");
-  li.appendChild(document.createTextNode(newItem))
+  li.appendChild(document.createTextNode(newItem));
 
+  // Create Button get from  out Function
   const button = createButton("remove-item btn-link text-red");
-li.appendChild(button)
-itemList.appendChild(li)
-itemInput.value="";
+  li.appendChild(button);
+
+  itemList.appendChild(li);
+  itemInput.value="";
 }
 
-function createButton(classes){
-const button = document.createElement("button")
-button.className = classes;
-const icon = createIcon("fa-solid fa-xmark")
-button.append(icon);
-return button
-}
-function createIcon(classes){
-const icon=document.createElement("i")
-icon.className=classes
-return icon
+// Create Button
+function createButton(classes) {
+  const button = document.createElement("button");
+  button.className = classes;
+  const icon = createIcon("fa-solid fa-xmark");
+  button.appendChild(icon);
+  return button;
 }
 
-// Event LIstner 
-itemForm.addEventListener("submit",addItem)
+function createIcon(classes) {
+  const icon = document.createElement("i");
+  icon.className = classes;
+
+  return icon;
+}
+
+itemForm.addEventListener("submit", addItem);
