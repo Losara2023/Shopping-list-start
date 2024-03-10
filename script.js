d0d8eee64d32896d5bf9f675d5535fd9f5b7f8b8
Losara2023 <arabest2023@gmail.com>
2024-03-10 23:11:00 +0100

    activate clear button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
+const clearBtn = document.getElementById("clear");
 
 function addItem(e) {
   e.preventDefault();
@@ -20,7 +21,7 @@ function addItem(e) {
   li.appendChild(button);
 
   itemList.appendChild(li);
-  itemInput.value="";
+  itemInput.value = "";
 }
 
 // Create Button
@@ -39,4 +40,21 @@ function createIcon(classes) {
   return icon;
 }
 
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    e.target.parentElement.parentElement.remove();
+  }
+}
+
+function clearItems(){
+  // first and easy way
+  // itemList.innerHTML=""
+  // Second way
+  while(itemList.firstChild){
+    itemList.removeChild(itemList.firstChild);
+  }
+}
+
 itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click",clearItems);
